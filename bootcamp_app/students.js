8fc73e96d1a7f2f6a202c5d72f9cefc152f5389d
RickCarr <rick.carr@telus.com>
2022-12-14 22:47:27 -0800

    sanitize teachers.js and students.js to prevent SQL Injection via LHL walkthrough

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,14 +7,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const [nodeVer, file, month, results] = process.argv
-pool.query(`
+const [nodeVer, file, month, limit] = process.argv;
+const inputs = [`%${month}%`, limit];
+const noHaxQuery = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts on cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${month}%'
-LIMIT ${results};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+pool.query(noHaxQuery,inputs)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,20 +8,22 @@ const pool = new Pool({
 });
 
 const [nodeVer, file, month] = process.argv;
-pool.query(`
+const cohort = [`%${month}%`];
+const noHaxQuery = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${month}%'
+WHERE cohorts.name LIKE $1
 GROUP BY cohort, teacher
 ORDER BY teacher;
-`)
+`;
+
+pool.query(noHaxQuery, cohort)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
     });
-
   })
   .catch(err => console.error('query error', err.stack));
